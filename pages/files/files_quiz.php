1445a6d0316bc9152cbdde5c430185ff4479839f
Justin Stein <justein230@gmail.com>
2018-05-02 23:47:14 -0400

    Big boi commit. Nav, quiz fixes.

--- pages/files/files_quiz.php
+++ pages/files/files_quiz.php
@@ -1,7 +1,7 @@
 <?php
 	$title='PALTS : File Permission Quiz';
 	$path='../../';
-	$nav='file_permission';
+	$nav='files_quiz';
 	include($path.'assets/inc/head.php');
 ?>
   <h2 class="quizHeader">Take a Quiz!</h2>
@@ -10,106 +10,106 @@
 		<div class="quizQuestion">
 			<p class="question">1.What does the command 'head -n 2' do?</p>
 
-		<input class="answer" type="radio" name="q1" value="1">
+		<input class="answer" type="radio" name="q1" value="1" id="q1a1">
 		<label id="correctString1" for="q1a1">Prints the first two lines of the file.</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a2">
 		<label for="q1a2">Deletes the first two lines of the file.</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a3">
 		<label for="q1a3">Inserts two lines into the head of the file.</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a4">
 		<label for="q1a4">Deletes the file.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">2. What does the 'locate c-' command do?</p>
 
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a1">
 		<label for="q2a1">Locates all the files in the system.</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a2">
 		<label for="q2a2">Locates the specified file in the system.</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="1">
+		<input class="answer" type="radio" name="q2" value="1" id="q2a3">
 		<label id="correctString2" for="q2a3">Prints the number of files returned instead of the name.</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a4">
 		<label for="q2a4">It doesn't do anything.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">3.Why use the 'find' command?</p>
 
-		<input class="answer" type="radio" name="q3" value="0">
+		<input class="answer" type="radio" name="q3" value="0" id="q3a1">
 		<label for="q3a1">To find the files you are looking for.</label>
 		<br>
-		<input class="answer" type="radio" name="q3" value="1">
+		<input class="answer" type="radio" name="q3" value="1" id="q3a2">
 		<label id="correctString3" for="q3a2">To find out about the file hierarchy.</label>
 		<br>
-		<input class="answer" type="radio" name="q3" value="0">
+		<input class="answer" type="radio" name="q3" value="0" id="q3a3">
 		<label for="q3a3">To get more info about each file.</label>
 		<br>
-		<input class="answer" type="radio" name="q3" value="0">
+		<input class="answer" type="radio" name="q3" value="0" id="q3a4">
 		<label for="q3a4">To find a good place to eat for lunch.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">4.What does the tar command stand for?</p>
 
-		<input class="answer" type="radio" name="q4" value="1">
+		<input class="answer" type="radio" name="q4" value="1" id="q4a1">
 		<label id="correctString4" for="q4a1">Tape archive.</label>
 		<br>
-		<input class="answer" type="radio" name="q4" value="0">
+		<input class="answer" type="radio" name="q4" value="0" id="q4a2">
 		<label for="q4a2">Tape archive resources.</label>
 		<br>
-		<input class="answer" type="radio" name="q4" value="0">
+		<input class="answer" type="radio" name="q4" value="0" id="q4a3">
 		<label for="q4a3">Technical archives.</label>
 		<br>
-		<input class="answer" type="radio" name="q4" value="0">
+		<input class="answer" type="radio" name="q4" value="0" id="q4a4">
 		<label for="q4a4">Tapes and records.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">5.The zip command zips the file name specified together.</p>
 
-		<input class="answer" type="radio" name="q5" value="1">
+		<input class="answer" type="radio" name="q5" value="1" id="q5a1">
 		<label id="correctString5" for="q5a1">True.</label>
 		<br>
-		<input class="answer" type="radio" name="q5" value="0">
+		<input class="answer" type="radio" name="q5" value="0" id="q5a2">
 		<label for="q5a2">False.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">6.What is the difference between the head and tail command?</p>
 
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a1">
 		<label for="q6a1">Nothing they do the same thing.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a2">
 		<label for="q6a2">The head shows the bottom of the page and the tail shows the top of the page.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="1">
+		<input class="answer" type="radio" name="q6" value="1" id="q6a3">
 		<label id="correctString6" for="q6a3">The head commands displays the number of lines specified at the top of the page; the tail command displays the number of lines specified at the bottom of the page.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a4">
 		<label for="q6a4">The head command adds to the file and the tail command deletes from the file.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">7.How do file commands make life easier?</p>
 
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a1">
 		<label for="q7a1">It doesn't the command line is too hard!</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="1">
+		<input class="answer" type="radio" name="q7" value="1" id="q7a2">
 		<label id="correctString7" for="q7a2">Because you can take a glimpse of a file without opening it.</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a3">
 		<label for="q7a3">File commands don't do anything.</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a4">
 		<label for="q7a4">Because you can add and delete as many files as you want.</label>
 		</div>
 
--- pages/permissions/permissions_quiz.php
+++ pages/permissions/permissions_quiz.php
@@ -10,100 +10,100 @@
 		<div class="quizQuestion">
 			<p class="question">1.What permissions do 754 stand for?</p>
 
-		<input class="answer" type="radio" name="q1" value="1">
+		<input class="answer" type="radio" name="q1" value="1" id="q1a1">
 		<label for="q1a1">user: rwx, group: rwx, other: r--</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a2">
 		<label id="correctString1" for="q1a2">user: rwx, group: r-x, other: r--</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a3">
 		<label for="q1a3">user: -wx, group: --x, other: --x</label>
 		<br>
-		<input class="answer" type="radio" name="q1" value="0">
+		<input class="answer" type="radio" name="q1" value="0" id="q1a4">
 		<label for="q1a4">user: r-x, group: rwx, other: rwx</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">2. What does the command 'chown root:palts file.txt' do?</p>
 
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a1">
 		<label for="q2a1">It sets file.txt's group to root and its user to palts</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="1">
+		<input class="answer" type="radio" name="q2" value="1" id="q2a2">
 		<label for="q2a2">It sets the user of file.txt to both root and palts</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a3">
 		<label id="correctString2" for="q2a3">It sets file.txt's user to root and its group to palts</label>
 		<br>
-		<input class="answer" type="radio" name="q2" value="0">
+		<input class="answer" type="radio" name="q2" value="0" id="q2a4">
 		<label for="q2a4">It sets the group of file.txt to both root and palts</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">3. A file can have more than one user attached as the 'user' of the file.</p>
 
-		<input class="answer" type="radio" name="q3" value="0">
+		<input class="answer" type="radio" name="q3" value="0" id="q3a1">
 		<label for="q3a1">True</label>
 		<br>
-		<input class="answer" type="radio" name="q3" value="1">
+		<input class="answer" type="radio" name="q3" value="1" id="q3a2">
 		<label id="correctString3" for="q3a2">False</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">4. There is a list somewhere on the system that defines who 'other' is for files and folders.</p>
 
-		<input class="answer" type="radio" name="q4" value="0">
+		<input class="answer" type="radio" name="q4" value="0" id="q4a1">
 		<label for="q4a1">True</label>
 		<br>
-		<input class="answer" type="radio" name="q4" value="1">
+		<input class="answer" type="radio" name="q4" value="1" id="q4a2">
 		<label id="correctString4" for="q4a2">False</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">5.What does the command 'chown user file.txt' do?</p>
 
-		<input class="answer" type="radio" name="q5" value="0">
+		<input class="answer" type="radio" name="q5" value="0" id="q5a1">
 		<label for="q5a1">Changes the name of the file to file.txt.</label>
 		<br>
-		<input class="answer" type="radio" name="q5" value="0">
+		<input class="answer" type="radio" name="q5" value="0" id="q5a2">
 		<label id="correctString5" for="q5a2">Changes the user of file.txt to 'user'.</label>
 		<br>
-		<input class="answer" type="radio" name="q5" value="1">
-		<label id="correctString5" for="q5a3">Creates a new file called 'file.txt' with the owner 'user'.</label>
+		<input class="answer" type="radio" name="q5" value="1" id="q5a3">
+		<label for="q5a3">Creates a new file called 'file.txt' with the owner 'user'.</label>
 		<br>
-		<input class="answer" type="radio" name="q5" value="0">
+		<input class="answer" type="radio" name="q5" value="0" id="q5a4">
 		<label for="q5a4">Creates a new user and a new file.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">6.What does the command 'groupadd students' do?</p>
 
-		<input class="answer" type="radio" name="q6" value="1">
+		<input class="answer" type="radio" name="q6" value="1" id="q6a1">
 		<label for="q6a1">Adds new members to the stuents group.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a2">
 		<label for="q6a2">Displays the members of the student group.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a3">
 		<label id="correctString6" for="q6a3">Adds a group called students to the system.</label>
 		<br>
-		<input class="answer" type="radio" name="q6" value="0">
+		<input class="answer" type="radio" name="q6" value="0" id="q6a4">
 		<label for="q6a4">Deltes the group called students.</label>
 		</div>
 
 		<div class="quizQuestion">
 			<p class="question">7.How many permissions are there?</p>
 
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a1">
 		<label for="q7a1">5</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="1">
+		<input class="answer" type="radio" name="q7" value="1" id="q7a2">
 		<label id="correctString7" for="q7a2">3</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a3">
 		<label for="q7a3">1</label>
 		<br>
-		<input class="answer" type="radio" name="q7" value="0">
+		<input class="answer" type="radio" name="q7" value="0" id="q7a4">
 		<label for="q7a4">16</label>
 		</div>
 
